0513fde33561e60b63faa9ae7e5bf7b1e30dde72
firstoliudi <first.l11101990@gmail.com>
2024-04-10 17:16:55 -0700

    Initial Commit

---
+++ QuestionnaireMaker/QuestionnaireMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionnaireMakerApp.swift
+//  QuestionnaireMaker
+//
+//  Created by csuftitan on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionnaireMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
